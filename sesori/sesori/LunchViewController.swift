a492ace24d9dc92b9ff67957125f1f24c3681bbd
jungwoo Lee <b5nnnal88@gmail.com>
2021-05-21 13:38:18 +0900

    Success show lunch data of date picker

--- sesori/sesori/LunchViewController.swift
+++ sesori/sesori/LunchViewController.swift
@@ -16,34 +16,38 @@ struct MealServiceDietInfo: Codable {
     let row: [Row]?
 }
 struct Row: Codable {
-    let ddishNm, orplcInfo: String
+    let ddishNm : String
 
     enum CodingKeys: String, CodingKey {
         case ddishNm = "DDISH_NM"
-        case orplcInfo = "ORPLC_INFO"
     }
     
     init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         ddishNm = (try? values.decode(String.self, forKey: .ddishNm))!
-        orplcInfo = (try? values.decode(String.self, forKey: .orplcInfo))!
     }
     
 }
 
 class LunchViewController : UIViewController {
     
-    @IBOutlet weak var TodayLunchLabel : UILabel!
+    @IBOutlet weak var todayLunchLabel : UILabel!
+    @IBOutlet weak var lunchDatePicker : UIDatePicker!
     
     override func viewDidLoad() {
-        getLunchData()
+        getLunchData(pickDate: "20210521")
     }
     
-    func getLunchData(){
-        //        let dateformatter = DateFormatter()
-        //        dateformatter.dateFormat = "yyyyMMdd"
-        //        let todaydate : String = dateformatter.string(from: Date())
-        let url = "https://open.neis.go.kr/hub/mealServiceDietInfo?Type=json&pIndex=1&pSize=1&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010537&MLSV_YMD=20210518&KEY=406a6783d8db4d5483fd44abf25d720f"
+    @IBAction func didLunchDatePickerValueChanged(){
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd"
+        let userPickDate = formatter.string(from: lunchDatePicker.date)
+        getLunchData(pickDate: userPickDate)
+    }
+    
+    func getLunchData(pickDate : String){
+        
+        let url = "https://open.neis.go.kr/hub/mealServiceDietInfo?Type=json&pIndex=1&pSize=1&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010537&MLSV_YMD="+pickDate+"&KEY=406a6783d8db4d5483fd44abf25d720f"
         
         AF.request(url,
                    method: .get).responseJSON{
@@ -52,7 +56,6 @@ class LunchViewController : UIViewController {
                     let lunch = try? decoder.decode(Lunch.self, from: response.data!)
                     let lunchdata = "\(String(describing: lunch))"
                     self.convertLunchText(todaylunch: lunchdata)
-                    print(lunch!)
                    }
     }
     
@@ -60,7 +63,14 @@ class LunchViewController : UIViewController {
     func convertLunchText(todaylunch : String){
         var converter : String = todaylunch
         converter = converter.replacingOccurrences(of: "<br/>", with: "\n")
-        self.TodayLunchLabel.text = converter
+        let arr = converter.components(separatedBy: "\"")
+        if arr[0] == "nil" {
+            converter = "해당 날짜는 급식이 없습니다!"
+        }
+        else if arr[1] != nil{
+            converter = arr[1]
+        }
+        self.todayLunchLabel.text = converter
     }
     
     
