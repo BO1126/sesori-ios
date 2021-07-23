942e1a6f59deebdaad83b6759cacf8948c5e06c9
jungwoo Lee <b5nnnal88@gmail.com>
2021-07-23 20:47:48 +0900

    apply tableview in SettingViewController

---
+++ sesori/sesori/SettingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingViewController.swift
+//  sesori
+//
+//  Created by 이정우 on 2021/07/23.
+//
+
+import UIKit
+
+class SettingViewController : UITableViewController {
+    
+    @IBAction func dismissView(){
+        dismiss(animated: true)
+    }
+    
+}
