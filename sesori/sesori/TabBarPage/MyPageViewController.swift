b811a34d2f41a5bccbde4045c4c67be1b8cc2de0
jungwoo Lee <b5nnnal88@gmail.com>
2021-11-17 11:34:24 +0900

    Change eagle icon and Implement Mypage

--- sesori/sesori/TabBarPage/MyPageViewController.swift
+++ sesori/sesori/TabBarPage/MyPageViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class MyPageViewController: UIViewController {
     
     @IBOutlet weak var mainView : UIView!
+    @IBOutlet weak var subView : UIView!
+    @IBOutlet weak var studentNumberLabel : UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +20,18 @@ class MyPageViewController: UIViewController {
         mainView.layer.masksToBounds = true
         mainView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
         // Do any additional setup after loading the view.
+        
+        subView.layer.borderWidth = 2
+        subView.layer.borderColor = CGColor(red: 104/255, green: 180/255, blue: 1, alpha: 1)
+        subView.layer.cornerRadius = 20
+        subView.layer.masksToBounds = true
+        subView.clipsToBounds = true
+        
+        if let studentNumber = UserDefaults.standard.string(forKey: "studentNumber"){
+            studentNumberLabel.text = studentNumber
+        }else{
+            studentNumberLabel.text = "00000"
+        }
     }
     
 
